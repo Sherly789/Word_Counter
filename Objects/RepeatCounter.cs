8f43e2c00ce895a050d1c0b9acdd5d45c3f673bf
Sherly789 <sherlysh789>
2016-07-24 10:33:05 -0700

    pass the second specs

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -7,11 +7,13 @@ namespace WordCounter
   {
     //properties
     private string _word1;
+    private string _word2;
 
     //constructors, getters and setters
-    public RepeatCounter(string word1)
+    public RepeatCounter(string word1, string word2)
     {
       _word1 = word1;
+      _word2 = word2;
     }
 
     public string GetWord1()
@@ -22,6 +24,16 @@ namespace WordCounter
     public void SetWord1(string word1)
     {
       _word1 = word1;
+    }
+
+    public string GetWord2()
+    {
+      return "hi";
+    }
+
+    public void SetWord2(string word2)
+    {
+      _word2 = word2;
     }
     //other methods
 
--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -5,14 +5,27 @@ namespace WordCounter
 {
   public class RepeatCounterTest
   {
+    // [Fact]
+    // public void GetWord_OneWordInput_true()
+    // {
+    //   string word1 = "hello";
+    //   RepeatCounter testNewWord = new RepeatCounter(word1);
+    //   string  testWord1 = testNewWord.GetWord1();
+    //
+    //   Assert.Equal(word1, testWord1);
+    // }
+
     [Fact]
-    public void GetWord_OneWordInput_true()
+    public void GetWord_TwoWordInput_true()
     {
       string word1 = "hello";
-      RepeatCounter testNewWord = new RepeatCounter(word1);
+      string word2 = "hi";
+      RepeatCounter testNewWord = new RepeatCounter(word1, word2);
       string  testWord1 = testNewWord.GetWord1();
+      string testWord2 = testNewWord.GetWord2();
 
       Assert.Equal(word1, testWord1);
+      Assert.Equal(word2, testWord2);
     }
   }
 }
