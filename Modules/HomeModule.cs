0ddf7119257b78f9e54af5a4f9a03f3dab410652
Sherly789 <sherlysh789>
2016-07-24 22:44:51 -0700

    creat two cshtml files

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -7,7 +7,16 @@ namespace WordCounter
   {
     public HomeModule()
     {
-      Get["/hi"] = _ => "hello";
+      Get["/"] = _ => View["index.cshtml"];
+
+      Post["/result"] = _ => {
+        RepeatCounter testNewWord = new RepeatCounter (
+          Request.Form["new-word"],
+          Request.Form["new-phrase"]
+        );
+        return View["result.cshtml", testNewWord];
+      };
+
     }
   }
 }
